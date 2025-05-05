68b0ea3cea57ca54e3fb816894e4328d20c5a2d2
d-magaki <dkmk.03112600@gmail.com>
2025-05-05 19:58:36 +0900

    案件管理の削除機能を作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :projects, only: [:index, :new, :create, :show, :edit, :update] do
+  resources :projects, only: [:index, :new, :create, :show, :edit, :update, :destroy] do
     member do
       delete "delete_attachment/:attachment_id", to: "projects#delete_attachment", as: :delete_attachment
     end
